e4dea16955e6a9a9b5aabf2a1f3175c43f5a13f2
PulokSec <riopulok@gmial.com>
2021-05-02 22:15:17 +0600

    email service added

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -6,7 +6,7 @@ const Contact = () => {
     function sendEmail(e) {
         e.preventDefault();
 
-    emailjs.sendForm('gmail', 'template_xb1w0ux', e.target, 'user_xdo4igsr6KSG1RwowIgJn')
+    emailjs.sendForm('service_98sa3i6', 'template_xb1w0ux', e.target, 'user_xdo4igsr6KSG1RwowIgJn')
         .then((result) => {
             console.log(result.text);
         }, (error) => {
@@ -15,23 +15,23 @@ const Contact = () => {
         e.target.reset()
     }
     return (
-        <div>
+        <div className="contact">
         <div className="container">
         <form onSubmit={sendEmail}>
-                <div className="py-5 contact py-70">
-                    <div className="col-8 form-group mx-auto">
+                <div className="py-5 py-70">
+                    <div className="col-9 form-group mx-auto">
                         <input type="text" className="form-control" placeholder="Name" name="name"/>
                     </div>
-                    <div className="col-8 form-group pt-2 mx-auto">
+                    <div className="col-9 form-group pt-2 mx-auto">
                         <input type="email" className="form-control" placeholder="Email Address" name="email"/>
                     </div>
-                    <div className="col-8 form-group pt-2 mx-auto">
+                    <div className="col-9 form-group pt-2 mx-auto">
                         <input type="text" className="form-control" placeholder="Subject" name="subject"/>
                     </div>
-                    <div className="col-8 form-group pt-2 mx-auto">
+                    <div className="col-9 form-group pt-2 mx-auto">
                         <textarea className="form-control" id="" cols="30" rows="8" placeholder="Your message" name="message"></textarea>
                     </div>
-                    <div className="col-8 pt-3 mx-auto">
+                    <div className="col-9 pt-3 mx-auto">
                         <input type="submit" className="btn btn-brand" value="Send Message"></input>
                     </div>
                 </div>
